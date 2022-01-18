c94f1a5d3bf29add5bb047f09c1d3e8192a7a8c0
Xavier Ouimet <x.ouimet@gmail.com>
2022-01-18 18:05:22 -0500

    feat: added syntax highlighting support

--- md2sf.py
+++ md2sf.py
@@ -6,6 +6,9 @@ import os
 import click
 import mistune
 from mistune.scanner import escape
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import html
 
 
 class sf_html_render(mistune.HTMLRenderer):
@@ -20,12 +23,18 @@ class sf_html_render(mistune.HTMLRenderer):
         """override the default paragraph handler"""
         return "<p>" + text + "</p>\n"
 
-    def block_code(self, code, info=None):
+    def block_code(self, code, lang=None):
         """inject the correct `code` block handlers for styling"""
-        html = '<pre class="ckeditor_codeblock"'
-        if info is not None:
-            info = info.strip()
-        return html + ">" + escape(code) + "</pre>\n"
+        html_start = '<pre class="ckeditor_codeblock">'
+        if lang:
+            lexer = get_lexer_by_name(lang, stripall=True)
+            formatter = html.HtmlFormatter()
+            return html_start + highlight(code, lexer, formatter)+ "</pre>\n"
+        else:
+            return html_start + escape(code) + "</pre>\n"
+
+        
+        #return '<pre><code>' + mistune.escape(code) + '</code></pre>'
 
 
 @click.command(help="Plese supply the path to a KB article in Markdown format to parse")
@@ -35,11 +44,17 @@ def main(filename):
     with open(filename) as sf_kb:
         sf_kb_file = sf_kb.read()
 
-    markdown = mistune.create_markdown(renderer=sf_html_render())
-
     sf_html = open(os.path.splitext(filename)[0] + ".html", "w")
-    sf_html.write(markdown(sf_kb_file))
 
+    with open('/home/xouimet/Documents/Dev/Random_scripts/markdown2salesforce/cssdata.txt') as sf_kb:
+        css_start = sf_kb.read()
+    sf_html.write(css_start)
+
+    markdown = mistune.create_markdown(renderer=sf_html_render())
+ 
+    
+    sf_html.write(markdown(sf_kb_file))
+    sf_html.write('\n</body>\n</html>')
 
 if __name__ == "__main__":
     main()
